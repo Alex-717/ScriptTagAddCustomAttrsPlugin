afbc3a30bac4bbf19466ad6c516ec440438c6690
Alex-717 <liangyuhao717@163.com>
2024-12-09 19:24:07 +0800

    dev

--- scriptRetryApendChild.js
+++ scriptRetryApendChild.js
@@ -7,8 +7,27 @@
     var __main_chunks__ = window.__main_chunks__
     var asyncChunks = []
     __all_chunks__.forEach(item => {
-      __main_chunks__
+      if (__main_chunks__.indexOf(item) === -1) {
+        asyncChunks.push(item)
+      }
     })
+
+    function isAsyncScript (src) {
+      var tag = false
+      for (var i = 0; i < asyncChunks.length; i++) {
+        var d = asyncChunks[i]
+        if (src.indexOf(d) > -1) {
+          tag = true
+          break
+        }
+      }
+      return tag
+    }
+
+    // 是异步加载的js
+    if (newChild.tagName.toLowerCase() === 'script' && isAsyncScript(newChild.src)) {
+      
+    }
 
     return originalAppendChild.call(this, newChild);
   };
